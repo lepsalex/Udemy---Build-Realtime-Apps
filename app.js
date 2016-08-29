45f9959ecb27747c535b2ae59dd2851f395a84e6
Alex Lepsa <lepsalex@gmail.com>
2016-08-28 21:05:22 -0400

    Section 2, Lecture 8

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// Temp
-let channels = [
-  {name: 'Hardware Support'},
-  {name: 'Software Support'}
-];
-
 // Channel
 class Channel extends React.Component {
   onClick() {
@@ -52,6 +46,7 @@ class ChannelForm extends React.Component {
     this.setState({
       channelName: ''
     })
+    this.props.addChannel(channelName);
   }
 
   render() {
@@ -68,11 +63,29 @@ class ChannelForm extends React.Component {
 
 // ChannelSection
 class ChannelSection extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        channels: [
+                    {name: 'Hardware Support'},
+                    {name: 'Software Support'}
+                  ]
+      };
+  }
+
+  addChannel(name) {
+    let {channels} = this.state;
+    channels.push({name: name});
+    this.setState({
+      channels: channels
+    });
+  }
+
   render() {
     return(
       <div>
-        <ChannelList channels={channels} />
-        <ChannelForm />
+        <ChannelList channels={this.state.channels} />
+        <ChannelForm addChannel={this.addChannel.bind(this)} />
       </div>
     );
   }
